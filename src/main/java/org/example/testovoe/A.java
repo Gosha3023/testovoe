c8f744d9a15d10e6fbcf588059aad18172998239
Енин Георгий <enin.gi@reliab.tech>
2023-01-09 12:35:18 +0400

    addbranch123

---
+++ src/main/java/org/example/testovoe/A.java
@@ -0,0 +1,5 @@
+package org.example.testovoe;
+
+public interface A {
+    void setA(int a);
+}
---
+++ src/main/java/org/example/testovoe/С.java
@@ -0,0 +1,6 @@
+package org.example.testovoe;
+
+abstract class C {
+    int a;
+    abstract void setR(int r);
+}
